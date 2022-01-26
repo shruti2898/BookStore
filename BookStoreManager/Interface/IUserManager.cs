224ffcd0098c71fc847fa4c0eac070e04b3ba891
Shruti <shrutisablaniya93@gmail.com>
2022-01-26 18:29:41 +0530

    [Shruti] Added : User api methods and stored procedures

---
+++ BookStoreManager/Interface/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using BookStoreModels.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreManager.Interface
+{
+    public interface IUserManager
+    {
+        Task<UserRegistrationModel> Register(UserRegistrationModel user);
+        Task<string> Login(UserCredentialsModel user);
+        Task<bool> ForgotPassword(string email);
+        Task<bool> ResetPassword(UserCredentialsModel user);
+    }
+}
---
+++ BookStoreManager/Manager/UserManager.cs
@@ -0,0 +1,66 @@
+﻿using BookStoreManager.Interface;
+using BookStoreModels.UserModel;
+using BookStoreRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreManager.Manager
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepository repository;
+        public UserManager(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<UserRegistrationModel> Register(UserRegistrationModel user)
+        {
+            try
+            {
+                return await this.repository.Register(user);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<string> Login(UserCredentialsModel user)
+        {
+            try
+            {
+                return await this.repository.Login(user);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> ForgotPassword(string email)
+        {
+            try
+            {
+                return await this.repository.ForgotPassword(email);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> ResetPassword(UserCredentialsModel user)
+        {
+            try
+            {
+                return await this.repository.ResetPassword(user);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
